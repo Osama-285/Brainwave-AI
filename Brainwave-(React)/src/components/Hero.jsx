1be9fa81b05f95234912f938c0fd4e27df7e5b4d
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-05-02 22:52:18 +0500

    complete hero section

--- Brainwave-(React)/src/components/Hero.jsx
+++ Brainwave-(React)/src/components/Hero.jsx
@@ -1,15 +1,16 @@
 import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
 import Section from "./Section";
-// import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
-// import { heroIcons } from "../constants";
-// import { ScrollParallax } from "react-just-parallax";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
 import { useRef } from "react";
 // import Generating from "./Generating";
 // import Notification from "./Notification";
 // import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
   return (
     <Section
       className="pt-[12rem] -mt-[5.25rem]"
@@ -18,12 +19,12 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative">
+      <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of AI Chatting with Brainwave
+            Explore the Possibilities of AI Chatting with {` `}
             <span className="inline-block relative">
-              Brainwave{" "}
+              Brainwave
               <img
                 src={curve}
                 className="absolute top-full left-0 w-full xl:-mt-2"
@@ -53,8 +54,18 @@ const Hero = () => {
                   height={490}
                   alt="AI"
                 />
+                <ScrollParallax isAbsolutelyPositioned>
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt={icon} />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
               </div>
             </div>
+            <Gradient />
           </div>
           <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
@@ -65,6 +76,7 @@ const Hero = () => {
               alt="hero"
             />
           </div>
+          <BackgroundCircles />
         </div>
       </div>
     </Section>
